a8139994066d2dd8ef5b2e8ddee6b69b3d9ce84a
Nkosi Enciso Givhan <koasterman@gmail.com>
2018-07-11 03:49:57 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,29 +1,39 @@
 # Write your code here.
 katz_deli = []
 
-def line(name)
-  if line.size == 0 
-    puts "The line is currently empty."
-  else
-    line 
-  end
-end
-
-
 def take_a_number(line, name)
-  line = line << (name)
-  puts  `Welcome, #{name}. You are number #{line} in line.`
+  line = line.push(name)
+  return  `Welcome, #{name}. You are number #{line} in line.`
 end
 
 
-def now_serving (line)
-  if line.size == 0 
-    puts "There is nobody waiting to be served!"
-  elsif line.size > 0 
-    temp = line.pop
-    puts "Currently serving #{temp}"
+def now_serving(line)
+  if line.size == 0
+    return"There is nobody waiting to be served!"
+  else line.size > 0
+    var temp = line[0]
+    line.shift
+    return `Currently serving #{temp}.`
   end
 end
 
 
+def current_line(line)
+   temp = "The line is currently:"
+  if line.size == 0
+    return "The line is currently empty."
+  else  
+    i = 0 
+    while i < line.size
+      if i != line.size - 1
+        temp = temp + ` #{i + 1}. #{line[i]},`
+      elsif i == line.size - 1
+        temp = temp + ` #{i + 1}. #{line[i]}`
+      end
+      i+=1
+  end
+      return temp
+end  
+end
+end
 
