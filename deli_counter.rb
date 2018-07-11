16278e4d86a55848705fa4784e48859243046a36
Nkosi Enciso Givhan <koasterman@gmail.com>
2018-07-11 03:27:40 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,33 +1,35 @@
 # Write your code here.
-var katzDeliLine = [];
+katzDeliLine = []
 
-function takeANumber(line, name){
+def takeANumber(line, name)
   line = line.push(name)
-  return  `Welcome, ${name}. You are number ${line} in line.`
-}
+  return  `Welcome, #{name}. You are number #{line} in line.`
 
-function nowServing(line){
-  if (line.length === 0){
+
+def nowServing(line)
+  if line.length == 0
     return"There is nobody waiting to be served!"
-  } if (line.length > 0){
+  elsif line.length > 0
     var temp = line[0]
-    line.shift()
-    return `Currently serving ${temp}.`
-  }
-}
+    line.shift
+    return `Currently serving #{temp}.`
+  end
+end
 
-function currentLine(line){
-  var temp = "The line is currently:"
-  if (line.length === 0){
+
+def currentLine(line)
+   temp = "The line is currently:"
+  if line.length == 0
     return "The line is currently empty."
-  } if (line.length > 0) {
-    for (var i = 0; i < line.length; i++){
-      if (i != line.length - 1){
-        temp = temp + ` ${i + 1}. ${line[i]},`
-      }
-      if (i === line.length - 1){
-        temp = temp + ` ${i + 1}. ${line[i]}`
-      }
-    }return temp
-  }
-}
+  elsif line.length > 0 
+    i = 0 
+    while i < line.size
+      if i != line.length - 1
+        temp = temp + ` #{i + 1}. #{line[i]},`
+      elsif (i === line.length - 1){
+        temp = temp + ` #{i + 1}. #{line[i]}`
+      end
+      i+=1
+    return temp
+  end
+end  
