5e00711fbb97cb4591f577f98661123c230bacec
Nkosi Enciso Givhan <koasterman@gmail.com>
2018-07-11 03:36:21 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,35 +4,6 @@ katz_deli = []
 def take_a_number(line, name)
   line = line.push(name)
   return  `Welcome, #{name}. You are number #{line} in line.`
-
-
-def now_serving(line)
-  if line.size == 0
-    return"There is nobody waiting to be served!"
-  else line.size > 0
-    var temp = line[0]
-    line.shift
-    return `Currently serving #{temp}.`
-  end
 end
 
-
-def current_line(line)
-   temp = "The line is currently:"
-  if line.size == 0
-    return "The line is currently empty."
-  else  
-    i = 0 
-    while i < line.size
-      if i != line.size - 1
-        temp = temp + ` #{i + 1}. #{line[i]},`
-      elsif i == line.size - 1
-        temp = temp + ` #{i + 1}. #{line[i]}`
-      end
-      i+=1
-  end
-      return temp
-end  
-end
-end
 
